71c40ac9599e02b8f0d6d8639f50e414bbae6897
Filippo scarry <49690013+ScarryP@users.noreply.github.com>
2019-11-28 22:43:32 +0100

    chore: have fields Frozen into PDF

--- src/utils/create-pdf.js
+++ src/utils/create-pdf.js
@@ -1,8 +1,7 @@
-import React from "react";
-
 import { navigate } from "@reach/router";
 
-import { PDFDocument, StandardFonts, rgb, fontkit } from "pdf-lib";
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
 
 import pdfLetterDefault from "./modulo_consegna.pdf";
 
@@ -12,37 +11,70 @@ const downloadPdf = pdfBytes => {
   navigate(blobUrl);
 };
 
-async function createPdf(item) {
-  console.log(item);
+function customSplit(str, maxLength) {
+  if (str.length <= maxLength) return str;
+  var reg = new RegExp(".{1," + maxLength + "}", "g");
+  var parts = str.match(reg);
+  return parts;
+}
 
+async function createPdf(item) {
   const existingPdfBytes = await fetch(pdfLetterDefault).then(res =>
     res.arrayBuffer()
   );
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
-  const JuraFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const HelveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
   pdfDoc.registerFontkit(fontkit);
 
   const pages = pdfDoc.getPages();
   const page = pages[0];
-  const fontSize = 24;
+  const fontSize = 12;
 
-  const textWidth = JuraFont.widthOfTextAtSize(item.owner, fontSize);
+  const Luogo =
+    "Berbenno di Valtellina";
 
   const black = rgb(0 / 255, 0 / 255, 0 / 255);
+  const { height } = page.getSize();
 
-  const { width, height } = page.getSize();
-  const centerX = width / 2;
-  const centerY = height / 2 - 250;
-  page.setFont(JuraFont);
+  page.setFont(HelveticaFont);
   page.setFontColor(black);
-  page.drawText(item.owner,
-    {
-      x: centerX - textWidth / 2,
-      y: centerY - 15
-    });
+  page.setFontSize(12);
 
+  page.drawText(item.owner, {
+    x: 160,
+    y: height - 175
+  });
+  page.drawText(item.CF, {
+    x: 160,
+    y: height - 195
+  });
+
+  page.setFontSize(10);
+  page.drawText(item.device, {
+    x: 80,
+    y: height - 500
+  });
+  page.drawText(customSplit(item.model, 22).join("\n"), {
+    x: 190,
+    y: height - 498,
+    lineHeight: 13
+  });
+  page.drawText(item.serial, {
+    x: 310,
+    y: height - 500,
+  });
+
+  page.setFontSize(11);
+  page.drawText(Luogo, {
+    x: 80,
+    y: height - 650,
+  });
+  page.drawText(item.dateFrom, {
+    x: 80,
+    y: height - 665,
+  });
 
   const pdfBytes = await pdfDoc.save();
   downloadPdf(pdfBytes);
