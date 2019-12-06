2fabfcc4be0242025f706d51061fca6b1d703ceb
Filippo scarry <49690013+ScarryP@users.noreply.github.com>
2019-12-06 12:26:07 +0100

    chore: wrap logo in a link to list

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,25 +1,47 @@
 import React from "react";
 
 import styled from "styled-components";
+
+import { Link } from "@reach/router";
+import { IconButton } from "office-ui-fabric-react";
+
 import Logo from "../logo";
 
 const ContainerDiv = styled.div`
   @media (max-width: 1200px) {
     margin: 96px 72px;
   }
+
   @media (max-width: 768px) {
-    margin: 96px 32px;
+    margin: 32px;
+    margin-top: 0;
   }
+
   margin: 96px 192px;
   justify-content: center;
 `;
 
-const Container = ({children}) => {
-  return(
-    <ContainerDiv>
-      <Logo/>
-      {children}
-    </ContainerDiv>
+const LogsContainer = styled.div`
+  position: absolute;
+  top: 24px;
+  right: 24px;
+`;
+
+const Container = ({ children }) => {
+  return (
+    <>
+      <LogsContainer>
+        <Link to="/app/logs">
+          <IconButton iconProps={{ iconName: "History" }} />
+        </Link>
+      </LogsContainer>
+      <ContainerDiv>
+        <Link to="/list">
+          <Logo />
+        </Link>
+        {children}
+      </ContainerDiv>
+    </>
   );
 };
 
